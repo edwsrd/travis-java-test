09afc91e7a0743806ce0e5fc36922999b5ca26ee
edwsrd <edwardxfm1@gmail.com>
2019-05-13 21:11:35 -0500

    Update Calculator.java
    cambio de letras a numeros

--- src/main/java/com/mayab/calidad/travis/Calculator.java
+++ src/main/java/com/mayab/calidad/travis/Calculator.java
@@ -2,7 +2,7 @@ package com.mayab.calidad.travis;
 
 public class Calculator {
 	public int add(int a, int b) {
-		return jkhj + b;
+		return a + b;
 	}
 	public int sub(int a, int b) {
 		return a - b;
